77b5e7313f309b5d262a9977f1ff96d5b00c49ed
Honglin Bao <baohongl@msu.edu>
2020-03-24 10:17:33 -0400

    Update loadmodel_evosearch.py

--- loadmodel_evosearch.py
+++ loadmodel_evosearch.py
@@ -1,12 +1,10 @@
-import sys
-import random
-import multiprocessing
-import numpy
-from deap import creator, base, tools, algorithms
+# evolutionary search through deap, not finished
+
 from random import randint
 from pickle import load
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
+#from deap import creator, base, tools, algorithms
  
 # load doc into memory
 def load_doc(filename):
@@ -17,8 +15,30 @@ def load_doc(filename):
 	# close the file
 	file.close()
 	return text
-
-
+ 
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
+	result = list()
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+		result.append(out_word)
+	return ' '.join(result)
+ 
 # load cleaned text sequences
 in_filename = 'cleaned.txt'
 doc = load_doc(in_filename)
@@ -30,6 +50,12 @@ model = load_model('model.h5')
  
 # load the tokenizer
 tokenizer = load(open('tokenizer.pkl', 'rb'))
-
-# evolutionary search through deap, not finished
+ 
+# select a seed text
+seed_text = lines[randint(0,len(lines))]
+print(seed_text + '\n')
+ 
+# generate new text
+generated = generate_seq(model, tokenizer, seq_length, seed_text, 50)
+print(generated)
 
