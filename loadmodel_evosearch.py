0e440b6e1ce6305d62586067f92da3b3d4f2a8b6
Honglin Bao <baohongl@msu.edu>
2020-03-23 22:39:45 -0400

    Update loadmodel_evosearch.py

--- loadmodel_evosearch.py
+++ loadmodel_evosearch.py
@@ -20,7 +20,7 @@ def load_doc(filename):
 
 
 # load cleaned text sequences
-#in_filename = 'republic_sequences.txt'
+in_filename = 'claened.txt'
 doc = load_doc(in_filename)
 lines = doc.split('\n')
 seq_length = len(lines[0].split()) - 1
