24e6163f42991fb4b9949c78770f20d813ec432d
Honglin Bao <baohongl@msu.edu>
2020-03-24 20:43:30 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -78,6 +78,20 @@ def to_lower(line):
 	logging.debug(line)
 	return ' '.join([w if w in TOKENS else w.lower() for w in line.split()])
 
+def clean_doc(doc):
+	# replace '--' with a space ' '
+	doc = doc.replace('--', ' ')
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', string.punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# make lower case
+	tokens = [word.lower() for word in tokens]
+	return tokens
+
 def separate_tokens(line):
 	logging.debug(line)
 	for TOKEN in TOKENS:
