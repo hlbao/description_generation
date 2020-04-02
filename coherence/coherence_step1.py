e25daec55f93e9acc47071f11c2361bebd16ca1a
Honglin Bao <baohongl@msu.edu>
2020-04-02 14:34:01 -0400

    Update coherence_step1.py

--- coherence/coherence_step1.py
+++ coherence/coherence_step1.py
@@ -3,8 +3,12 @@ import pandas as pd
 import os
 import re
 import gensim
-
+import spacy
+import nltk
+from gensim.models import CoherenceModel
+from nltk.corpus import stopwords
 from gensim.utils import simple_preprocess
+import gensim.corpora as corpora
 
 os.chdir('..')
 # Read data into papers
@@ -19,5 +23,66 @@ def sent_to_words(sentences):
         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
 data = papers.paper_text_processed.values.tolist()
 data_words = list(sent_to_words(data))
-print(data_words[:1])
+#print(data_words[:1])
 
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# NLTK Stop words
+# nltk.download('stopwords')
+
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+# Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    #"""https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+# Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+# Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+# Initialize spacy 'en' model, keeping only tagger component (for efficiency)
+nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+# Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+print(data_lemmatized[:1])
+
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_lemmatized)
+# Create Corpus
+texts = data_lemmatized
+# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+# View
+#print(corpus[:1])
+
+
+# Build LDA model
+lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                       id2word=id2word,
+                                       num_topics=10, 
+                                       random_state=100,
+                                       chunksize=100,
+                                       passes=10,
+                                       per_word_topics=True)
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
