a7fb4cf8e38c74130904975584d55a33406d60e3
Honglin Bao <baohongl@msu.edu>
2020-04-02 14:11:27 -0400

    Update coherence_step1.py

--- coherence/coherence_step1.py
+++ coherence/coherence_step1.py
@@ -1,9 +1,12 @@
 # Importing modules
 import pandas as pd
 import os
+import re
+
+
 os.chdir('..')
 # Read data into papers
-papers = pd.read_csv('./data/NIPS Papers/papers.csv')
+papers = pd.read_csv('./data/papers.csv')
 # Print head
 papers.head()
 # sample only 2 - for demonstration purposes
