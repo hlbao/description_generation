2c0adf54b1b4b96d3e6a0ed467a6db06e0fefe3d
Honglin Bao <baohongl@msu.edu>
2020-04-02 14:24:29 -0400

    Update coherence_step1.py

--- coherence/coherence_step1.py
+++ coherence/coherence_step1.py
@@ -2,14 +2,22 @@
 import pandas as pd
 import os
 import re
+import gensim
 
+from gensim.utils import simple_preprocess
 
 os.chdir('..')
 # Read data into papers
 paper = pd.read_csv('./dataset1and2/paper.csv')
-# Print head
-paper.head()
+
 # sample only 2 - for demonstration purposes
 paper = paper.sample(2)
-# Print out the first rows of papers
-papers.head()
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+data = papers.paper_text_processed.values.tolist()
+data_words = list(sent_to_words(data))
+print(data_words[:1])
+
