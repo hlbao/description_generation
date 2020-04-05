b9da2a817a332e9c4120d062e744db5876ae5ed2
Honglin Bao <baohongl@msu.edu>
2020-04-05 12:34:13 -0400

    Update coherence.py

--- coherence/coherence.py
+++ coherence/coherence.py
@@ -94,6 +94,11 @@ lda_model = gensim.models.LdaMulticore(corpus=corpus,
 
 # Compute Coherence Score
 coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_p')
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_uci')
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_umass')
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_npmi')
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_a')
 coherence_lda = coherence_model_lda.get_coherence()
 print('\nCoherence Score: ', coherence_lda)
 
