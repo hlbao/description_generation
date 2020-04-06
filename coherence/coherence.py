5ec1e198822a16316ca257c283c709f05ae675d0
Honglin Bao <baohongl@msu.edu>
2020-04-06 16:23:17 -0400

    Update coherence.py

--- coherence/coherence.py
+++ coherence/coherence.py
@@ -30,10 +30,6 @@ data_words = list(sent_to_words(data))
 # print(data_words[:1][0][:100])
 # see output_1.txt
 
-# Build the bigram and trigram models
-bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
-trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
-# Faster way to get a sentence clubbed as a trigram/bigram
 bigram_mod = gensim.models.phrases.Phraser(bigram)
 trigram_mod = gensim.models.phrases.Phraser(trigram)
 
